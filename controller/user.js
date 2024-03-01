051216d4bd9c9ac81184e1fe63d86b10783b0e2a
EarthStrix <78458755+EarthStrixDEV@users.noreply.github.com>
2024-03-01 16:09:06 +0700

    getUser with bodyJson

--- controller/user.js
+++ controller/user.js
@@ -16,6 +16,21 @@ router.get('/getAllUser' ,async(request ,response) => {
     }
 })
 
+router.post('/getUser',async(request ,response) => {
+    const {email} = request.body
+    try {
+        const query = await pool.query('SELECT * FROM mykuusertable WHERE email = ?',[email])
+        const result = await query[0]
+        response.json(result)
+    } catch (error) {
+        console.error(error);
+        response.json({
+            status: 'error',
+            message: error
+        })
+    }
+})
+
 router.post('/register' ,async(request ,response) => {
     const {name ,lastname ,email ,password} = request.body
     try {
